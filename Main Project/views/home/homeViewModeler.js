30bc1d1f870cf5dd7256986da3fe94bacebc70d2
Cosmin <cosmin.aftanase@yahoo.com>
2020-05-18 22:59:25 +0300

    Added dynamic searchBlocks to home page

--- Main Project/views/home/homeViewModeler.js
+++ Main Project/views/home/homeViewModeler.js
@@ -1,4 +1,6 @@
-let selectParent = document.getElementById('emptySelectParent');
+// let searchBlock0 = document.getElementById('searchBlock0');
+let searchBlocks = [document.getElementById('searchBlockStart')]
+let searchBlockNo = 0;
 let selectGenerator = document.getElementById('addSelect'); 
 const selectMap = { 
   "Source" : ['MapQuest', 'Bing'],
@@ -45,7 +47,9 @@ const nameToFunc = {
   "Astronomical_Twilight" : newCheckbox
 }
 
-function Intialize(){ 
+
+//Adds all options to "selectGenerator" select from nameToFunc map
+function IntializeSelectGenerator(){ 
   for(var name in nameToFunc) {
     let option = document.createElement("option");
     option.value = name;
@@ -54,6 +58,8 @@ function Intialize(){
   }
 }
 
+
+//Collomn name + remove button to select
 function addTextToElement(element, text){
   let span = document.createElement("span");
   let button = document.createElement("button"); 
@@ -72,18 +78,19 @@ function addTextToElement(element, text){
     br.remove();
     button.remove();
   }
-  element.parentNode.insertBefore(button, colorPicker.nextSibling);
+  element.parentNode.insertBefore(button, element.nextSibling);
 
   //Break line
   element.parentNode.insertBefore(br, button.nextSibling);
 } 
 
+
 //Creates a new html select 
 function newSelect(){
-  //Intialize variables
+  //Intialize variable
   let select = document.createElement("select"); 
   select.name = addSelect.value;
-  selectParent.appendChild(select);
+  searchBlocks[searchBlockNo].appendChild(select);
 
   //Values of select
   var values = selectMap[addSelect.value]
@@ -96,56 +103,85 @@ function newSelect(){
     select.appendChild(option);
   }
 
+  //Add data around this input
   addTextToElement(select, select.name);
 }
 
+
 //Creates a new html date 
 function newDate(){
-  //Intialize variables
+  //Intialize variable
   let date = document.createElement("input");
   date.name = addSelect.value; 
   date.type = "date";
+
+  //Add lower and upper bound based on dateMap map
   date.min = dateMap[date.name][0];
   date.max = dateMap[date.name][1];
-  selectParent.appendChild(date); 
+
+  //Add it to current searchBlock
+  searchBlocks[searchBlockNo].appendChild(date); 
+
+  //Add data around this input
   addTextToElement(date, date.name);
 }
 
+
 //Creates a new html checkbox 
 function newCheckbox(){
+   //Intialize variable
   let checkBox = document.createElement("input");
   checkBox.name = addSelect.value; 
   checkBox.type = "checkbox"; 
-  selectParent.appendChild(checkBox); 
+
+  //Add it to current searchBlock 
+  searchBlocks[searchBlockNo].appendChild(checkBox); 
+
+  //Add data around this input
   addTextToElement(checkBox, checkBox.name);
 }
  
-function createColorPicker(){
+
+// Appends a new searchBlock to the last searchBlock
+function createBlock(){
   //Get this element
-  let todo = document.getElementById("todo");
+  let createBlock = document.getElementById("createBlock");
 
   //Add color picker 
   let colorPicker = document.createElement("input"); 
   colorPicker.type = "color";
   colorPicker.name = "color";
   colorPicker.className = "colorPicker";
-  colorPicker.onchange = function(){  
-    //Add the button back
-    let button = todo.cloneNode(true);
-    
+
+  //Replace createBlock button with color picker
+  createBlock.parentNode.insertBefore(colorPicker, createBlock); 
+  createBlock.remove();
+
+  //When choosing color, color picker will create the actual search block and recreate the "createBlock" button
+  colorPicker.onchange = function(){ 
+
+    //Add the search block
+    newSearchBlock = document.createElement("div");
+    newSearchBlock.id = "searchBlock" + searchBlockNo;
+    lastSearchBlock = searchBlocks[searchBlockNo]; 
+    lastSearchBlock.parentNode.insertBefore(newSearchBlock, lastSearchBlock.nextSibling); 
+
+    //Add to our array
+    searchBlockNo += 1;
+    searchBlocks.push(newSearchBlock);
+
+    //Add the collor
+    newSearchBlock.style.backgroundColor = colorPicker.value;
+
+    //Add the "createBlock" button back
+    let button = createBlock.cloneNode(true); 
     selectGenerator.parentNode.insertBefore(button, selectGenerator.nextSibling);
 
-    //Remove this element
-    colorPicker.remove();
-
-    //todo:
-    //add this below
+    //Remove color picker
+    colorPicker.remove(); 
   }
-  todo.parentNode.insertBefore(colorPicker, todo); 
- 
-  //Remove this element
-  todo.remove();
 }  
+
 
 // Event listeners 
 selectGenerator.addEventListener('change', function() {
@@ -153,40 +189,64 @@ selectGenerator.addEventListener('change', function() {
   const func = nameToFunc[addSelect.value];
   func()
   addSelect.value='0';
-}); 
+});  
 
-selectGenerator.addEventListener('change', function() {
-  console.log(addSelect.value)
-  const func = nameToFunc[addSelect.value];
-  func()
-  addSelect.value='0';
-}); 
 
 //Used when submit button is pressed
-function makeSearch() {  
-  //Get the form from html
-  const form = document.getElementById('form');
-  let values = '';
-  
-  //Copy each value from form into values
-  for ( var i = 0; i < form.elements.length - 1; i++ ) {
-    var e = form.elements[i]; 
-    if (e.type == "checkbox")
-    { 
-      values = values + e.name + "=" + e.checked + "&";
+function makeSearch() { 
+  //We make a different search for every searchBlock   
+  for (var index = 0; index <= searchBlockNo; index++)
+  {  
+    //Get the block from searchBlocks list
+    let block = searchBlocks[index]; 
+
+    //If block is empty continue
+    if (block.childNodes.length == 0)
       continue;
-    }
-    if (e.className == "removeButton")
-      continue;
-    values = values + e.name + "=" + e.value + "&";
-  } 
-  
-  //Call function
-  values = values.slice(0, -1);
-  console.log(values);
-  //getReq(values);
+
+    //Copy each value from that searchBlock into queryString
+    let queryString = "";
+    for ( var i = 0; i < block.childNodes.length; i++ ) {
+      //Get the elements from that block
+      var e = block.childNodes[i];  
+
+      //If checkbox we are interested in true/false value
+      if (e.type == "checkbox")
+      { 
+        queryString = queryString + e.name + "=" + e.checked + "&";
+        continue;
+      }
+
+      //If is a remove button/div/span we skip it
+      if (e.className == "removeButton" || e.value == undefined)
+      {
+        continue;
+      }
+
+      //If is text area/date that was uncompleted we simply ignore it (and show a message)
+      if (e.value == ""){
+        alert("empty data in " + e.name + " will not be sent to server!");
+        continue;
+      }
+
+      //We get the name and values and add it to our query string
+      queryString = queryString + e.name + "=" + e.value + "&";
+    } 
+    
+    //Add the color
+    let color = "rgb(0, 0, 0)";
+    if (searchBlocks[index].id != "searchBlockStart")
+      color =  searchBlocks[index].style.backgroundColor;
+    queryString = queryString + "col=" + color;
+
+    //Print the result locally + send it to server
+    console.log("For block number " + index + " we have: " + queryString);
+    //getReq(values);
+  }
+
   return false;
 }  
+
 
 //Get data from db
 function getReq(params) {
@@ -202,4 +262,4 @@ function getReq(params) {
 } 
 
 
-Intialize();
+IntializeSelectGenerator();
