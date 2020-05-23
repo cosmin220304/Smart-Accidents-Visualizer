1f52d52cfcafdedf8e0062da2da82bb6622b2fe7
Alex <megadonee6@gmail.com>
2020-05-24 00:27:21 +0300

    added heatMapViewModeler

--- Main Project/views/heatMap/heatMap.js
+++ Main Project/views/heatMap/heatMap.js
@@ -1,55 +1,161 @@
-      google.charts.load('current', {'packages':['geochart']});
-      google.charts.setOnLoadCallback(drawRegionsMap);
+ google.charts.load('current', {'packages':['geochart']});
+ google.charts.setOnLoadCallback(drawRegionsMap);
+//Used when submit button is pressed
 
-      function drawRegionsMap() {
+function makeSearch() { 
+    //Copy each value from that searchBlock into queryString
+    let queryString = "";
+    var block = document.getElementById("searchBlockStart");
+    for ( var i = 0; i < block.childNodes.length; i++ ) {
+      //Get the elements from that block
+      var e = block.childNodes[i];  
 
-        var data = google.visualization.arrayToDataTable([
-          ['State', 'Select'],
-          ['AL', 10],
-          ['AK', 120],
-          ['AR', 10],
-          ['AK', 10],
-          ['AZ', 10],
-          ['Colorado', 0],
-          ['CO', 150],
-          ['DE', 120],
-          ['FL', 130],
-          ['HI', 120],
-          ['KS', 110],
-          ['KY', 180],
-          ['MI', 110],
-          ['MO', 180],
-          ['MS', 10],
-          ['MT', 101],
-          ['NE', 120],
-          ['NJ', 1210],
-          ['NM', 1210],
-          ['NY', 1410],
-          ['OR', 1310],
-          ['PA', 1510],
-          ['TX', 1610],
-          ['UT', 1710],
-          ['VA', 1810],
-          ['WA', 1910],
-          ['WV', 1910],
-          ['WY', 1910],
-        ]);
-
-        var options = {
-          colorAxis: {colors: ['#FEFFD2', '#EFFF00', '#910000']},
-          region: 'US',
-          displayMode: 'regions',
-          resolution: 'provinces',
-          backgroundColor: 'transparent',
-          chartArea: {
-            backgroundColor: 'transparent',
-          }
-         };
-
-        var chart = new google.visualization.GeoChart(document.getElementById('geochart'));
-
-        chart.draw(data, options);
+      //If checkbox we are interested in true/false value
+      if (e.type == "checkbox")
+      { 
+        queryString = queryString + e.name + "=" + e.checked + "&";
+        continue;
       }
 
+      //If is a remove button/div/span we skip it
+      if (e.className == "removeButton" || e.value == undefined)
+      {
+        continue;
+      }
 
+      //If is text area/date that was uncompleted we simply ignore it (and show a message)
+      if (e.value == ""){
+        alert("empty data in " + e.name + " will not be sent to server!");
+        continue;
+      }
+
+      //We get the name and values and add it to our query string
+      queryString = queryString + e.name + "=" + e.value + "&";
+    } 
+    qsToArr(queryString);
+  return false;
+}  
+async function qsToArr(queryString)
+{
+  console.log(queryString);
+  var dataJS = await getReq(queryString);
+  var coordonatesObject = Object.values(dataJS);
+  console.log(coordonatesObject);
+
+  var statesValueArr = [];
+  for (var i = 0; i< coordonatesObject.length; i++){
+    var stat = coordonatesObject[i]._id;;
+    var valoare = coordonatesObject[i].counter;
+    statesValueArr.push([stat, valoare])
+  }
+  drawRegionsMap(statesValueArr)
+}
+//Get data from db
+async function getReq(queryString) {
+  return new Promise((resolve, reject) => {
+    try {
+        fetch("http://127.0.0.1:8128/heatMap?" + queryString, {
+          method: 'GET',
+          headers: {
+            'Accept': 'application/json, */*',
+            'Content-type': 'application/json'
+          }, 
+        })
+        .then((res) => res.json())
+        .then((data) => {console.log(data); resolve(data);} );
+        
+    }
+    catch (error){
+        reject(error);
+    }
+  });
+} 
+ 
+  function drawRegionsMap(statesValueArr) {
+    if(statesValueArr != undefined)
+      console.log(statesValueArr);
+    var data = [
+      ['State', 'Select'],
+      ["AZ",0],
+      ["AL",0],
+      ["AK",0],
+      ["AR",0],
+      ["CA",0],
+      ["CO",0],
+      ["CT",0],
+      ["DC",0],
+      ["DE",0],
+      ["FL",0],
+      ["GA",0],
+      ["HI",0],
+      ["ID",0],
+      ["IL",0],
+      ["IN",0],
+      ["IA",0],
+      ["KS",0],
+      ["KY",0],
+      ["LA",0],
+      ["ME",0],
+      ["MD",0],
+      ["MA",0],
+      ["MI",0],
+      ["MN",0],
+      ["MS",0],
+      ["MO",0],
+      ["MT",0],
+      ["NE",0],
+      ["NV",0],
+      ["NH",0],
+      ["NJ",0],
+      ["NM",0],
+      ["NY",0],
+      ["NC",0],
+      ["ND",0],
+      ["OH",0],
+      ["OK",0],
+      ["OR",0],
+      ["PA",0],
+      ["RI",0],
+      ["SC",0],
+      ["SD",0],
+      ["TN",0],
+      ["TX",0],
+      ["UT",0],
+      ["VT",0],
+      ["VA",0],
+      ["WA",0],
+      ["WV",0],
+      ["WI",0],
+      ["WY",0],
+      ["AS",0],
+      ["GU",0],
+      ["MP",0],
+      ["PR",0],
+      ["VI",0],
+      ["UM",0]
+    ];
+    if(statesValueArr != undefined){
+      var states = Object.keys(statesValueArr);
+      for(var i = 0; i < states.length; i++)
+      {
+        console.log(data[i][0]);
+        console.log(states[i][0]);
+      }
+  }
+    //var data2 = google.visualization.arrayToDataTable();
+    var options = {
+      colorAxis: {colors: ['#FEFFD2', '#EFFF00', '#8B0000']},
+      region: 'US',
+      displayMode: 'regions',
+      resolution: 'provinces',
+      backgroundColor: 'transparent',
+      chartArea: {
+        backgroundColor: 'transparent',
+      }
+     }
+    var chart = new google.visualization.GeoChart(document.getElementById('geochart'))
+    chart.clearChart();
+
+    chart.draw(google.visualization.arrayToDataTable(data), options);
+}
 
---
+++ Main Project/views/heatMap/heatMapViewModeler.js
@@ -0,0 +1,242 @@
+// let searchBlock0 = document.getElementById('searchBlock0');
+let searchBlocks = [document.getElementById('searchBlockStart')]
+let searchBlockNo = 0;
+let selectGenerator = document.getElementById('addSelect'); 
+const selectMap = { 
+  "Source" : ['MapQuest', 'Bing'],
+  "Severity" : ['1', '2', '3', '4'],
+  "Side" : ['L', 'R'],
+  "State" : ["AZ","AL","AK","AR","CA","CO","CT","DC","DE","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY","AS","GU","MP","PR","VI","UM"],
+  "Country" : ["US"],
+  "Timezone" : ["US/Pacific", "US/Mountain", "US/Central", "US/Easter"]
+}
+const dateMap = {
+  "Start_Time" : ["2015-03-09", "2020-01-01"],
+  "End_Time" : ["2016-02-08", "2020-11-01"],
+  "Weather_Timestamp" : ["2016-02-08", "2020-01-01"]
+}
+const sliderMap = {
+  "Temperature(F)" : ["-78" , "167"],
+  "Wind_Chill(F)" : ["-54", "115"],
+  "Humidity(%)" : ["0", "100"],
+  "Pressure(in)" : ["0", "33"],
+  "Visibility(mi)" : ["0", "111"],
+  "Wind_Speed(mph)" : ["0", "823"],
+  "Precipitation(in)" : ["0", "25"],
+  "TMC" : ["200", "406"]
+}
+const nameToFunc = { 
+  "Source" : newSelect, 
+  "Severity" : newSelect,
+  "Side" : newSelect,
+  "State" : newSelect,
+  "Country" : newSelect,
+  "Timezone" : newSelect,
+  "Start_Time" : newDate,
+  "End_Time" : newDate,
+  "Weather_Timestamp" : newDate,
+  "Temperature(F)" : newSlider,
+  "Wind_Chill(F)" : newSlider,
+  "Humidity(%)" : newSlider,
+  "Pressure(in)" : newSlider,
+  "Visibility(mi)" : newSlider,
+  "Wind_Speed(mph)" :newSlider,
+  "Precipitation(in)": newSlider,
+  "TMC" : newSlider,  
+  "Distance(mi)" : newSlider,
+  "Amenity" : newCheckbox,
+  "Bump" : newCheckbox,
+  "Crossing" : newCheckbox,
+  "Give_Way" : newCheckbox,
+  "Junction" : newCheckbox,
+  "No_Exit": newCheckbox,
+  "Railway": newCheckbox,
+  "Roundabout" : newCheckbox,
+  "Station" : newCheckbox,
+  "Stop" : newCheckbox,
+  "Traffic_Calming" : newCheckbox,
+  "Traffic_Signal" : newCheckbox,
+  "Turning_Loop" : newCheckbox,
+  "Sunrise_Sunset" : newCheckbox,
+  "Civil_Twilight" : newCheckbox,
+  "Nautical_Twilight" : newCheckbox,
+  "Astronomical_Twilight" : newCheckbox
+}
+
+
+//Adds all options to "selectGenerator" select from nameToFunc map
+function IntializeSelectGenerator(){ 
+  for(var name in nameToFunc) {
+    let option = document.createElement("option");
+    option.value = name;
+    option.text = name;
+    selectGenerator.appendChild(option);
+  }
+}
+
+
+//Collomn name + remove button to select
+function addTextToElement(element, text){
+  let span = document.createElement("span");
+  let button = document.createElement("button"); 
+  const br = document.createElement("br");
+
+  let sliderText = undefined;
+  if (element.type == "range")
+    sliderText = element.nextSibling;
+
+  //Text before field
+  text = text.replace("_", " ") + " : ";
+  span.innerHTML = text; 
+  element.parentNode.insertBefore(span, element);
+
+  //Remove button
+  button.className = "removeButton";
+  button.onclick = function(){
+    span.remove();
+    element.remove();
+    br.remove();
+    if (!(sliderText === undefined))
+      sliderText.remove();
+    button.remove();
+  }
+  element.parentNode.insertBefore(button, element.nextSibling);
+
+  //Break line
+  element.parentNode.insertBefore(br, button.nextSibling);
+} 
+
+
+//Creates a new html select 
+function newSelect(){
+  //Intialize variable
+  let select = document.createElement("select"); 
+  select.name = addSelect.value;
+  searchBlocks[searchBlockNo].appendChild(select);
+
+  //Values of select
+  var values = selectMap[addSelect.value]
+ 
+  //Add data for that select
+  for (var i = 0; i < values.length; i++) {
+    let option = document.createElement("option");
+    option.value = values[i];
+    option.text = values[i];
+    select.appendChild(option);
+  }
+
+  //Add data around this input
+  addTextToElement(select, select.name);
+}
+
+
+//Creates a new html date 
+function newDate(){
+  //Intialize variable
+  let date = document.createElement("input");
+  date.name = addSelect.value; 
+  date.type = "date";
+
+  //Add lower and upper bound based on dateMap map
+  date.min = dateMap[date.name][0];
+  date.max = dateMap[date.name][1];
+
+  //Add it to current searchBlock
+  searchBlocks[searchBlockNo].appendChild(date); 
+
+  //Add data around this input
+  addTextToElement(date, date.name);
+}
+
+
+//Creates a new html checkbox 
+function newCheckbox(){
+   //Intialize variable
+  let checkBox = document.createElement("input");
+  checkBox.name = addSelect.value; 
+  checkBox.type = "checkbox"; 
+
+  //Add it to current searchBlock 
+  searchBlocks[searchBlockNo].appendChild(checkBox); 
+
+  //Add data around this input
+  addTextToElement(checkBox, checkBox.name);
+}
+
+function newSlider(){
+  //Creating slider
+  let slider = document.createElement("input");
+  slider.type="range";
+  slider.class="slider"; 
+  slider.name = addSelect.value;
+  slider.min= sliderMap[slider.name][0];
+  slider.max= sliderMap[slider.name][1];
+  slider.step="0.1"; 
+
+  //Creating slider text displaying values
+  let sliderText = document.createElement("span");   
+  sliderText.setAttribute("name", addSelect.value); 
+  sliderText.innerHTML = slider.value;
+  slider.oninput = ()=> { sliderText.innerHTML = slider.value; }
+
+  //Add it to current searchBlock 
+  searchBlocks[searchBlockNo].appendChild(slider); 
+  slider.parentNode.insertBefore(sliderText, slider.nextSibling);
+
+  //Add data around this input
+  addTextToElement(slider, slider.name);
+}
+
+// Appends a new searchBlock to the last searchBlock
+// function createBlock(){
+//   //Get this element
+//   let createBlock = document.getElementById("createBlock");
+
+//   //Add color picker 
+//   let colorPicker = document.createElement("input"); 
+//   colorPicker.type = "color";
+//   colorPicker.name = "color";
+//   colorPicker.className = "colorPicker";
+
+//   //Replace createBlock button with color picker
+//   createBlock.parentNode.insertBefore(colorPicker, createBlock); 
+//   createBlock.remove();
+
+//   //When choosing color, color picker will create the actual search block and recreate the "createBlock" button
+//   colorPicker.onchange = function(){ 
+
+//     //Add the search block
+//     newSearchBlock = document.createElement("div");
+//     newSearchBlock.id = "searchBlock" + searchBlockNo;
+//     lastSearchBlock = searchBlocks[searchBlockNo]; 
+//     lastSearchBlock.parentNode.insertBefore(newSearchBlock, lastSearchBlock.nextSibling); 
+
+//     //Add to our array
+//     searchBlockNo += 1;
+//     searchBlocks.push(newSearchBlock);
+
+//     //Add the collor
+//     newSearchBlock.style.backgroundColor = colorPicker.value;
+
+//     //Add the "createBlock" button back
+//     let button = createBlock.cloneNode(true); 
+//     selectGenerator.parentNode.insertBefore(button, selectGenerator.nextSibling);
+
+//     //Remove color picker
+//     colorPicker.remove(); 
+//   }
+// }  
+
+
+// Event listeners 
+selectGenerator.addEventListener('change', function() {
+  console.log(addSelect.value)
+  const func = nameToFunc[addSelect.value];
+  func()
+  addSelect.value='0';
+});  
+
+
+
+
+IntializeSelectGenerator();
